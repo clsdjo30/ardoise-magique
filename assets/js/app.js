03ad182b16031e70601c1ec239adad032f15ae41
clsdjo30 <lesergent.cedric@gmail.com>
2025-12-12 07:38:34 +0100

    feat(design-system): add js script to toogle navbar on mobil

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,11 @@ import initDropdown from './components/dropdown.js';
 import initTabs from './components/tabs.js';
 import createToast from './components/toast.js';
 import SectionProlongement from './components/section-prolongement.js';
+import initNavbar from './components/navbar.js';
 import initHero from './hero.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    initNavbar();
     initModal();
     initDropdown();
     initTabs();
